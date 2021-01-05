0218f25b8d18c7c4e6dcb9ebed76e25a0e1dfe1f
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2021-01-05 22:02:09 +0400

    Add files via upload

---
+++ Tekrar/Tekrar/Extension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekrar
+{
+    public static class Extension
+    {
+        public static string CustomReplace(this string str,char oldChar,char newChar)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (char letter in str)
+            {
+                if(letter== oldChar)
+                {
+                    builder.Append(newChar);
+                }
+                else
+                {
+                    builder.Append(letter);
+                }
+            }
+            return builder.ToString();
+        }
+
+        public static int GetCount(this int[] arr,int number) //arr -> 10,20,30,10  number->10
+        {
+            int count = 0;
+            foreach (int item in arr)
+            {
+                if (item == number) count++;
+            }
+            return count;
+        }
+
+        public static int[] GetSameValue(this int[] arr1,int[] arr2)
+        {
+            int[] result = new int[0];
+            foreach (int i in arr1) //{ 1, 2, 3, 4, 5, 6 }
+            {
+                foreach (int j in arr2) //{ 2, 4, 6, 8, 10 }
+                {
+                    if (i == j)
+                    {
+                        Array.Resize(ref result, result.Length+1); //3
+                        result[result.Length-1] = i; //result[2]=i
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Tekrar/Tekrar/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Using Array.Sort for custom type array
+            //Person p1 = new Person("Fikret", 25);
+            //Person p2 = new Person("Kenan", 26);
+            //Person p3 = new Person("Kamran", 30);
+            //Person p4 = new Person("Miri", 21);
+            //Person[] people = { p3, p1, p2, p4 };
+
+            //Array.Sort(people);
+            //foreach (Person p in people)
+            //{
+            //    Console.WriteLine(p);
+            //}
+            #endregion
+
+            #region Task
+            //string word = "Hello";
+            //Console.WriteLine(word.CustomReplace('l','a'));
+
+            //int[] arr = { 10, 20, 30, 10 };
+            //Console.WriteLine(arr.GetCount(10));
+
+            int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+            int[] arr2 = { 2, 4, 6, 8, 10 };
+            Console.WriteLine(arr1.GetSameValue(arr2));
+            #endregion
+        }
+    }
+
+    #region Using Array.Sort for custom type array
+    class Person:IComparable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            return this.Age.CompareTo(((Person)obj).Age);
+        }
+    }
+    #endregion
+
+
+}
